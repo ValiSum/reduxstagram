b2d2493251cc00b15b79209c48620fdac575174b
Vali Sumurduc <vali_sum@outlook.com>
2019-04-15 18:40:41 +0200

    Add comment and remove comment functionality

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -8,7 +8,11 @@ const Comments = React.createClass({
         <p>
           <strong>{ comment.user }</strong>
           { comment.text }
-          <button className="remove-comment">&times;</button>
+          <button className="remove-comment" 
+            onClick={ () => this.props.removeComment(this.props.params.postId, i) }
+          >
+            &times;
+          </button>
         </p>
       </div>
     );
@@ -19,6 +23,7 @@ const Comments = React.createClass({
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   },
   render() {
     return (
